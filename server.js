70f421c41b35cef775b6d2ea42cedfc1b39a96b7
Harianto Rais <ibnurais.abdillah@gmail.com>
2026-03-15 22:43:30 +0700

    update post to 5050

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require("./src/app");
 require("dotenv").config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5050;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`=================================`);
