028aee7a2d6bca7d26a57a19362c83c6bc27b184
Harianto Rais <ibnurais.abdillah@gmail.com>
2026-03-16 11:20:13 +0900

    update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const PORT = process.env.PORT || 8081;
 
-app.listen(PORT, "127.0.0.1", () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`=================================`);
   console.log(`🚀 Server running on port ${PORT}`);
   console.log(`🔗 URL: http://localhost:${PORT}`);
